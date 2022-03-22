fixing backprop

src/activation_functions/Sigmoid.py
@@ -9,8 +9,8 @@ class Sigmoid(Module):
 
 	def backward_update_gradient(self, input, delta):
 		exp_ = np.exp(-input)
-		self._gradient += (exp_ / (1 + exp_)**2) @ delta
+		self._gradient += (exp_ / (1 + exp_)**2) * delta
 		return self._gradient
 
 	def backward_delta(self, input, delta):
-		return input @ delta
+		return input * delta

src/activation_functions/Softmax.py
@@ -12,17 +12,17 @@ class Softmax(Module):
 	def backward_update_gradient(self, input, delta):
 		input_size = input.shape[0]
 		input_vector = input.reshape(input_size, 1)
-		input_matrix = np.tile(input_vector, input_size) # np.tile repeats the <input_vector> array <input_size> times
+		input_matrix = np.tile(input_vector, input_size) ## np.tile repeats the <input_vector> array <input_size> times
 		res = np.diag(input) - (input_matrix * input_matrix.T)
-		self._gradient = res @ delta
+		self._gradient = res * delta
 		return self._gradient
 
 	def backward_delta(self, input, delta):
-		return input @ delta
+		return input * delta
 
-# Quotient rule for derivatives
-# Derivatives are 0 if i!=j and exp(x_j) else. (i and j are the index of the classes)
-# Evaluate the quotient rule for both cases
-# Find a way to implement it in python
-# We can do that by using the jacobian
-# We just need the diagonal so that's why we use np.diag()
+## Quotient rule for derivatives
+## Derivatives are 0 if i!=j and exp(x_j) else. (i and j are the index of the classes)
+## Evaluate the quotient rule for both cases
+## Find a way to implement it in python
+## We can do that by using the jacobian
+## We just need the diagonal so that's why we use np.diag()

src/activation_functions/Tanh.py
@@ -9,8 +9,8 @@ class Tanh(Module):
 
 	def backward_update_gradient(self, input, delta):
 		res = 1 - np.tanh(input)**2
-		self._gradient = res @ delta
+		self._gradient = res * delta
 		return self._gradient
 
 	def backward_delta(self, input, delta):
-		return input @ delta
+		return input * delta