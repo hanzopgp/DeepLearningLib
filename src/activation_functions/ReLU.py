adding some assertion and fixing gradients

src/activation_functions/ReLU.py
@@ -4,9 +4,12 @@ class ReLU(Module):
 	def forward(self, input):
 		self._input = input
 		self._output = np.maximum(0, self._input)
+		assert(self._input.shape == self._output.shape)
 
-	def backward_update_gradient(self, input, delta):
-		self._gradient += np.where(input > 0, 1, 0) * delta
+	def backward_update_gradient(self, delta):
+		assert(delta.shape == self._output.shape)
+		self._delta = delta
+		self._gradient += np.where(self._input > 0, 1, 0) * self._delta
 
-	def backward_delta(self, input, delta):
-		self._delta = input * delta
+	def backward_delta(self):
+		self._new_delta = self._input * self._delta

src/activation_functions/Sigmoid.py
@@ -4,10 +4,12 @@ class Sigmoid(Module):
 	def forward(self, input):
 		self._input = input
 		self._output = 1/(1 + np.exp(-self._input))
+		assert(self._input.shape == self._output.shape)
 
-	def backward_update_gradient(self, input, delta):
-		exp_ = np.exp(-input)
+	def backward_update_gradient(self, delta):
+		assert(delta.shape == self._output.shape)
+		exp_ = np.exp(-self._input)
 		self._gradient += (exp_ / (1 + exp_)**2) * delta
 
-	def backward_delta(self, input, delta):
-		self._delta = input * delta
+	def backward_delta(self):
+		self._new_delta = self._input * self._delta

src/activation_functions/Softmax.py
@@ -7,16 +7,19 @@ class Softmax(Module):
 		self._input = input
 		exp_ = np.exp(self._input)
 		self._output = exp_ / np.sum(exp_)
+		assert(self._input.shape == self._output.shape)
 
-	def backward_update_gradient(self, input, delta):
-		input_size = input.shape[0]
-		input_vector = input.reshape(input_size, 1)
+	def backward_update_gradient(self, delta):
+		assert(delta.shape == self._output.shape)
+		self._delta = delta
+		input_size = self._input.shape[0]
+		input_vector = self._input.reshape(input_size, 1)
 		input_matrix = np.tile(input_vector, input_size) ## np.tile repeats the <input_vector> array <input_size> times
-		res = np.diag(input) - (input_matrix * input_matrix.T)
+		res = np.diag(self._input) - (input_matrix * input_matrix.T)
 		self._gradient = res * delta
 
-	def backward_delta(self, input, delta):
-		self._delta = input * delta
+	def backward_delta(self):
+		self._new_delta = self._input * self._delta
 
 ## Quotient rule for derivatives
 ## Derivatives are 0 if i!=j and exp(x_j) else. (i and j are the index of the classes)

src/activation_functions/Tanh.py
@@ -4,10 +4,13 @@ class Tanh(Module):
 	def forward(self, input):
 		self._input = input
 		self._output = np.tanh(self._input)
+		assert(self._input.shape == self._output.shape)
 
-	def backward_update_gradient(self, input, delta):
-		res = 1 - np.tanh(input)**2
-		self._gradient = res * delta
+	def backward_update_gradient(self, delta):
+		assert(delta.shape == self._output.shape)
+		self._delta = delta
+		res = 1 - np.tanh(self._input)**2
+		self._gradient = res * self._delta
 
-	def backward_delta(self, input, delta):
-		self._delta = input * delta
+	def backward_delta(self):
+		self._new_delta = self._input * self._delta

src/layers/Linear.py
@@ -1,5 +1,8 @@
 from Core import *
 
+# Source: https://web.eecs.umich.edu/~justincj/teaching/eecs442/notes/linear-backprop.html
+
+
 class Linear(Module):
 	def __init__(self, size_in, size_out):
 		super().__init__()
@@ -11,16 +14,18 @@ class Linear(Module):
 		self._gradient = np.zeros_like(self._parameters)
 
 	def forward(self, input):
-		# print(input.shape)
-		# print(self._parameters.shape)
+		print(input.shape)
+		print(self._parameters.shape)
 		assert(input.shape[1] == self._parameters.shape[0])
-		assert(self._parameters.shape[1] == self._bias.shape[0])
 		self._input = input
 		self._output = self._input @ self._parameters + self._bias
 
-	def backward_update_gradient(self, grad_input, delta):
-		self._grad_input = grad_input
-		self._gradient += self._grad_input.T * self._delta 
+	def backward_update_gradient(self, delta):
+		assert(delta.shape == self._gradient.shape)
+		assert(delta.shape == self._output.shape)
+		self._delta = delta
+		self._gradient += self._input * self._delta 
+		assert(self._gradient.shape == self._parameters.shape)
 		
-	def backward_delta(self, input, delta):
-		self._delta = self._grad_input @ self._parameters * self._delta
+	def backward_delta(self):
+		self._new_delta = self._parameters * self._delta