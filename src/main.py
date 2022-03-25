853e017f04444584bcb98b10f4f70cf632c3cbb3
hanzo <enzodurandcontact@gmail.com>
2022-03-26 00:48:47 +0100

    fixed some erros and updated display, verbose

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ np.random.seed(42)
 
 
 if __name__ == '__main__':
-	data_generation = DataGeneration(points=2000, classes=2)
+	data_generation = DataGeneration(points=3000, classes=2)
 	data_generation.make_vertical_data()
 	# data_generation.display_data()
 	X, y = data_generation.x, data_generation.y
@@ -16,23 +16,26 @@ if __name__ == '__main__':
 	n_features = X.shape[1]
 	n_neurons = 64 
 	n_classes = len(np.unique(y))	
-	learning_rate = 1e-5
-	n_epochs = 5
-	train_split = 0.25
+	learning_rate = 1e-4
+	n_epochs = 10
+	train_split = 0.2
 
+	X, valid_x, y, valid_y = split_data(X, y, train_split=train_split, shuffle=True)
 	train_x, test_x, train_y, test_y = split_data(X, y, train_split=train_split, shuffle=True)
 
 	## Gradient exploded when going for 128 neurons per hidden layer and SGD
 	## Gradient exploded when going for GD
 	model = Sequential()
 	model.add(layer=Linear(n_features, n_neurons), activation="tanh")
+	# model.add(layer=Linear(n_neurons, n_neurons), activation="tanh")
 	model.add(layer=Linear(n_neurons, n_classes), activation="sigmoid")
 	model.compile(loss="sparse_binary_crossentropy", 
 				  optimizer="SGD",
 				  learning_rate=learning_rate)
 	model.summary()
 	model.fit(train_x, train_y, n_epochs=n_epochs)
+	# model.fit(train_x, train_y, valid_x, valid_y n_epochs=n_epochs)
 
 	model.stats()
-	print("--> Accuracy:", model.score(train_x, train_y, type="accuracy"))
-	print("--> Accuracy:", model.score(test_x, test_y, type="accuracy"))
+	print("--> Accuracy in train :", model.score(train_x, train_y, type="accuracy"))
+	print("--> Accuracy in test :", model.score(test_x, test_y, type="accuracy"))
--- src/module/Sequential.py
+++ src/module/Sequential.py
@@ -23,6 +23,7 @@ class Sequential(Module):
 		self.network = []
 		self.loss_values = []
 		self.acc_values = []
+		self._valid = False
 
 	def add(self, layer, activation):
 		self.network.append(layer)
@@ -52,6 +53,7 @@ class Sequential(Module):
 		elif loss == "sparse_crossentropy":
 			loss_function = SparseCrossEntropy()
 		## Choosing and optimizer function for our network
+		self._optimizer_name = optimizer
 		if optimizer == "GD":
 			self.optimizer = GradientDescent(self, loss_function, learning_rate)
 		elif optimizer == "SGD":
@@ -59,12 +61,22 @@ class Sequential(Module):
 		elif optimizer == "MGD":
 			self.optimizer = MinibatchGradientDescent(self, loss_function, learning_rate)
 
-	def fit(self, X, y, n_epochs):
-		self._X = X
-		self._y = y
+	def fit(self, *arg, n_epochs):
+		if len(arg) == 2:
+			self._X = arg[0]
+			self._y = arg[1]
+		elif len(arg) == 4:
+			self._X = arg[0]
+			self._y = arg[1]
+			self._valid_x = arg[2]
+			self._valid_y = arg[3]
+			self._valid = True
+		else:
+			print("Error : number of arguments in fit()")
 		for _ in range(n_epochs):
-			self.optimizer.step(X, self._y)
+			self.optimizer.step(self._X, self._y)
 			self.update_stats()
+			self.show_updates(valid=self._valid)
 			
 	def predict(self, X):
 		last_module = len(self.network) - 1
@@ -72,6 +84,10 @@ class Sequential(Module):
 		for i in range(1, last_module):
 			self.network[i].forward(self.network[i-1]._output)
 		return self.network[last_module - 1]._output
+
+	def show_updates(self, valid):
+		print(self.score(self._X, self._y, type="accuracy"))
+		if valid: print(self.score(self._valid_x, self._valid_y, type="accuracy"))
 
 	def update_stats(self):
 		last_module = len(self.network) - 1
@@ -92,16 +108,18 @@ class Sequential(Module):
 
 	def summary(self):
 		print("=========================================================================")
+		print("==> Network :")
 		for i, m in enumerate(self.network):
 			type_ = str(type(m))
 			element = type_.split(".")[-1][:-2]
 			if "layers" in type_:
-				print("Layer", int(i/2), ":", element, "with parameters_shape =", m._parameters.shape, end="")
+				print("--> Layer", int(i/2), ":", element, "with parameters_shape =", m._parameters.shape, end="")
 			elif "activation_functions" in type_:
 				print(" and activation :", element)
 			elif "loss_functions" in type_:
-				print("Loss :", element)
-		print("Total number of parameters :", self.count_parameters())
+				print("* Loss :", element)
+		print("* Optimizer :", self._optimizer_name)
+		print("* Total number of parameters :", self.count_parameters())
 		print("=========================================================================")
 
 	def count_parameters(self):
