fixed a problem in sigmoid derivative which made the gradient explod

src/activation_functions/Sigmoid.py
@@ -17,6 +17,6 @@ class Sigmoid(Module):
 		self._delta = delta
 
 	def backward_delta(self):
-		exp_ = np.exp(-self._input)
-		gradient = (exp_ / (1 + exp_)**2)
+		sigmoid = 1/(1 + np.exp(-self._input))
+		gradient = sigmoid * (1 - sigmoid)
 		self._new_delta = gradient * self._delta

src/activation_functions/Softmax.py
@@ -22,7 +22,8 @@ class Softmax(Module):
 	def backward_delta(self):
 		input_size = self._input.shape[0]
 		input_vector = self._input.reshape(input_size, 1)
-		input_matrix = np.tile(input_vector, input_size) ## np.tile repeats the <input_vector> array <input_size> times
+		## np.tile repeats the <input_vector> array <input_size> times
+		input_matrix = np.tile(input_vector, input_size) 
 		gradient = np.diag(self._input) - (input_matrix * input_matrix.T)
 		self._new_delta = gradient * self._delta
 

src/main.py
@@ -1,6 +1,6 @@
 from data.DataGeneration import DataGeneration
 from layers.Linear import Linear
-from module.Sequential import Sequential
+from network.Sequential import Sequential
 from utils.utils import *
 
 import numpy as np
@@ -28,9 +28,9 @@ if __name__ == '__main__':
 	X, y = data_generation.x, data_generation.y
 
 	n_features = X.shape[1]
-	n_neurons = 64 
+	n_neurons = 32 
 	n_classes = len(np.unique(y))	
-	learning_rate = 1e-4
+	learning_rate = 1e-5
 	n_epochs = 10
 	train_split = 0.2
 
@@ -39,17 +39,20 @@ if __name__ == '__main__':
 
 	## Gradient exploded when going for 128 neurons per hidden layer and SGD
 	## Gradient exploded when going for GD
+	## Gradient exploded when going for 2 hidden layers
+	## Going for a lower learning rate fixes the problem but I'm not sure its intended
 	model = Sequential()
 	model.add(layer=Linear(n_features, n_neurons), activation="tanh")
+	# model.add(layer=Linear(n_neurons, n_neurons), activation="tanh")
 	model.add(layer=Linear(n_neurons, n_classes), activation="sigmoid")
 	model.compile(loss="sparse_binary_crossentropy", 
 				  optimizer="SGD",
-				  learning_rate=learning_rate)
+				  learning_rate=learning_rate,
+				  metric="accuracy")
 	model.summary()
 	model.fit(train_x, train_y, valid_x, valid_y, n_epochs=n_epochs, verbose=True)
 
 	model.plot_stats()
-	_, train_acc = model.score(train_x, train_y, type="accuracy")
-	_, test_acc = model.score(test_x, test_y, type="accuracy")
-	print("--> Accuracy in train :", train_acc)
-	print("--> Accuracy in test :", test_acc)
+	# _, train_acc, _, test_acc = model.compute_scores(type="accuracy")
+	# print("--> Accuracy in train :", train_acc)
+	# print("--> Accuracy in test :", test_acc)

src/network/Sequential.py
@@ -26,6 +26,7 @@ class Sequential(Module):
 		self._valid_loss_values = []
 		self._valid_acc_values = []
 		self._valid = False
+		self._metric = None
 
 	############################################### USER FUNCTIONS ###############################################
 
@@ -42,7 +43,9 @@ class Sequential(Module):
 		else:
 			print("Error : wrong activation function")
 
-	def compile(self, loss, optimizer, learning_rate):
+	def compile(self, loss, optimizer, learning_rate, metric):
+		## Choosing a metric
+		self._metric = metric
 		## Choosing a loss function for our network
 		if loss == "binary_crossentropy":
 			loss_function = BinaryCrossEntropy()
@@ -134,14 +137,14 @@ class Sequential(Module):
 
 	def show_updates(self, cpt_epoch):
 		print("epoch :", cpt_epoch, end="")
-		train_loss, train_acc = self.score(self._X, self._y, type="accuracy")
+		train_loss, train_acc = self.compute_train_score()
 		print(", train_acc :", '{:<08g}'.format(train_acc), end="")
 		if train_loss < 0:
 			print(", train_loss :", '{:<2e}'.format(train_loss), end="")
 		else:
 			print(", train_loss :", " "+('{:<2e}'.format(train_loss)), end="")
 		if self._valid: 
-			valid_loss, valid_acc = self.score(self._valid_x, self._valid_y, type="accuracy")
+			valid_loss, valid_acc = self.compute_valid_score()
 			print(", valid_acc :", '{:<08g}'.format(valid_acc), end="")
 			if valid_loss < 0: 
 				print(", valid_loss :", '{:<2e}'.format(valid_loss), end="")
@@ -151,18 +154,36 @@ class Sequential(Module):
 
 	def update_stats(self):
 		## Updates train tracking
-		loss, acc = self.score(self._X, self._y, type="accuracy")
+		loss, acc = self.compute_train_score()
 		self._train_loss_values.append(loss)
 		self._train_acc_values.append(acc)
 		## Updates valid tracking
 		if self._valid:
-			loss, acc = self.score(self._valid_x, self._valid_y, type="accuracy")
+			loss, acc = self.compute_valid_score()
 			self._valid_loss_values.append(loss)
 			self._valid_acc_values.append(acc)
 
-	def score(self, X, y, type="accuracy"):
-		if type == "accuracy":
-			return self.network[len(self.network) - 1]._output.mean(), np.where(y == self.predict(X).argmax(axis=1), 1, 0).mean()
+	def compute_scores(self):
+		train_loss, train_acc = self.compute_train_score(self, type)
+		if self._valid:
+			valid_loss, valid_acc = self.compute_valid_score(self, type)
+			return train_loss, train_acc, valid_loss, valid_acc
+		return train_loss, train_acc
+
+	def compute_train_score(self):
+		loss = self.network[len(self.network) - 1]._output.mean()
+		if self._metric == "accuracy":
+			acc = np.where(self._y == self.network[len(self.network) - 2]._output.argmax(axis=1), 1, 0).mean()
+		return loss, acc
+
+	def compute_valid_score(self):
+		## Forward pass to update our network with validation data
+		self.forward(self._valid_x, self._valid_y)
+		## Returns the loss and the metrics for the validation data
+		loss = self.network[len(self.network) - 1]._output.mean()
+		if self._metric == "accuracy":
+			acc = np.where(self._valid_y == self.network[len(self.network) - 2]._output.argmax(axis=1), 1, 0).mean()
+		return loss, acc
 
 	def plot_stats(self):
 		## Show loss per epochs