fc29dddc634447270cb4460e3a989cbc5f061b80
Damien Dam <qdam@student.42.fr>
2022-04-06 15:43:46 +0200

    small addition

--- src/cnn/Conv.py
+++ src/cnn/Conv.py
@@ -2,4 +2,6 @@ from Core import *
 from global_imports import *
 
 class Convo1D(Module):
-	pass
+	def __init__(self, kernel_size, chan_in, chan_out, stride):
+		self._parameters = (np.random.rand(kernel_size, chan_in) * 2 - 1) / np.sqrt(chan_in)	# xavier-init
+		self._stride = stride
--- src/cnn/Pooling.py
+++ src/cnn/Pooling.py
@@ -2,4 +2,9 @@ from Core import *
 from global_imports import *
 
 class MaxPool1D(Module):
-	pass
+	def __init__(self, kernel_size, stride):
+		pass
+
+class Flatten(Module):
+	def __init__(self, batch, length, chan_in):
+		pass
