68136580fd7dfb4e258c45b68df604ab3aec11a9
Damien Dam <qdam@student.42.fr>
2022-04-09 12:41:32 +0200

    forward Conv1D Pooling and Flatten ok

--- src/cnn/Conv.py
+++ src/cnn/Conv.py
@@ -2,6 +2,39 @@ from Core import *
 from global_imports import *
 
 class Convo1D(Module):
-	def __init__(self, kernel_size, chan_in, chan_out, stride):
-		self._parameters = (np.random.rand(kernel_size, chan_in) * 2 - 1) / np.sqrt(chan_in)	# xavier-init
+	def __init__(self, kernel_size:int, chan_in:int, chan_out:int, stride:int):
+		self._parameters:np.ndarray = (np.random.rand(chan_out, kernel_size, chan_in) * 2 - 1) / np.sqrt(chan_in)	# xavier-init
 		self._stride = stride
+		self.zero_grad()
+
+	def zero_grad(self):
+		self._gradient = np.zeros_like(self._parameters)
+
+	def forward(self, inp:np.ndarray):
+		chan_out, ksz, chan_in = self._parameters.shape
+		batch_sz = inp.shape[0]
+		length = inp.shape[1]
+		assert inp.shape[2] == chan_in
+		d_out = int(np.floor((length - ksz) / self._stride)) + 1
+		self._input = inp
+		self._output = np.zeros((batch_sz, d_out, chan_out))
+
+		# Parallelizing may be possible here
+		idx_out = 0
+		for i in range(0, length, self._stride):
+			if idx_out == d_out:
+				break
+			for b in range(batch_sz):	# Need to find a way to process batch with numpy rather than loopoig here
+				window = inp[b,i:i+ksz,:]
+				self._output[b,idx_out,:] = np.sum(self._parameters * window, axis=(1,2))
+			idx_out += 1
+
+	# def update_parameters(self, learning_rate):
+	# 	self._parameters -= (learning_rate * self._gradient)
+
+	# def backward_update_gradient(self, delta):
+	# 	self._delta = delta
+	# 	self._gradient += self._input.T @ self._delta
+
+	# def backward_delta(self):
+	# 	self._new_delta = self._delta @ self._parameters.T
--- src/cnn/Pooling.py
+++ src/cnn/Pooling.py
@@ -2,9 +2,35 @@ from Core import *
 from global_imports import *
 
 class MaxPool1D(Module):
-	def __init__(self, kernel_size, stride):
-		pass
+	def __init__(self, kernel_size:int, stride:int):
+		self._ksz = kernel_size
+		self._stride = stride
+
+	def zero_grad(self):
+		self._gradient = np.zeros_like(self._gradient)
+
+	def forward(self, inp:np.ndarray):
+		if self._gradient is None:
+			self._gradient = np.zeros_like(inp)
+		batch_sz, length, chan = inp.shape
+		d_out = int(np.floor((length - self._ksz) / self._stride)) + 1
+		self._input = inp
+		self._output = np.zeros((batch_sz, d_out, chan))
+
+		idx_out = 0
+		for i in range(0, length, self._stride):
+			if idx_out == d_out:
+				break
+			for b in range(batch_sz):	# Need to find a way to process batch with numpy rather than loopoig here
+				window = inp[b,i:i+self._ksz,:]
+				self._output[b,idx_out,:] = np.max(window, axis=0)
+				argmax = np.argmax(window, axis=0)
+				self._gradient[b,argmax+i,np.arange(argmax.shape[0])] = 1
+			idx_out += 1
+
 
 class Flatten(Module):
-	def __init__(self, batch, length, chan_in):
-		pass
+	def forward(self, inp:np.ndarray):
+		batch_sz, length, chan = inp.shape
+		self._input = inp
+		self._output = inp.reshape((batch_sz, length * chan))
